4fd09807b0ee92815d9665a048d67cf6060626bf
younes <younestemar@gmail.com>
2021-06-09 12:00:18 +0200

    apdate enivornement prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: false,
+  apiUrl:'http://ressourcesrelationnelles-env-1.eba-mwm3jz3f.us-east-1.elasticbeanstalk.com/projectRE',
+  fileLocalUrl:'../../assets/FileAsset/'
 };
